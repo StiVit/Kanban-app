f6f652f1f8269c88172deeb7772f52d397a349d2
Vitalie Stinca <122181196+StiVit@users.noreply.github.com>
2025-04-01 17:36:42 +0100

    feat: Create blueprints for the CRUD operations

--- main.go
+++ main.go
@@ -19,25 +19,56 @@ func main() {
 
 	log.Println("Setting up routes...")
 
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(RouteResponse{Message: "Hello, World!"})
-	}).Methods("GET")
-
+	router.HandleFunc("/register", register).Methods("POST")
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/projects", createProject).Methods("POST")
+	router.HandleFunc("/projects/{id}", updateProject).Methods("PUT")
+	router.HandleFunc("/projects", getProjects).Methods("GET")
+	router.HandleFunc("/project/{id}", getProject).Methods("GET")
+	router.HandleFunc("/project/{id}", deleteProject).Methods("DELETE")
+ 
 	log.Println("Listening on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 // register
+func register(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from register"})
+}
 
-// login 
+// login
+func login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from login"})
+}
 
 // createProject
+func createProject(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from createProject"})
+}
 
 // updateProject
+func updateProject(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from updateProject"})
+}
 
 // getProjects
+func getProjects(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from getProjects"})
+}
 
 // getProject
+func getProject(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from getProject"})
+}
 
 // deleteProject
+func deleteProject(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from deleteProject"})
+}
