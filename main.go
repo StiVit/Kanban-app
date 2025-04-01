6c357a8108bbf7f6860ae5f565237d5de860ac02
vitalie stinca <vitalicstinca123@gmail.com>
2025-04-01 17:04:22 +0100

    feat: Set up basic loging

--- main.go
+++ main.go
@@ -13,13 +13,31 @@ type RouteResponse struct {
 }
 
 func main() {
+	log.Println("Starting server on :8080")
 	// Defining a router that will handle incoming HTTP requests and route them to the appropriate handlers
 	router := mux.NewRouter()
+
+	log.Println("Setting up routes...")
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(RouteResponse{Message: "Hello, World!"})
 	}).Methods("GET")
 
+	log.Println("Listening on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
+
+// register
+
+// login 
+
+// createProject
+
+// updateProject
+
+// getProjects
+
+// getProject
+
+// deleteProject
