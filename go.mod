b9a5b72138f362edd29b2df6b25609c367af64da
vitalie stinca <vitalicstinca123@gmail.com>
2025-04-02 15:22:07 +0100

    feat: Set up logging middleware for each router

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/StiVit/kanban-app
 go 1.24.1
 
 require github.com/gorilla/mux v1.8.1
+
+require github.com/justinas/alice v1.2.0 // indirect
--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/justinas/alice"
 )
 
 type RouteResponse struct {
 	Message string `json:"message"`
+	ID string `json:"id,omitempty"` // ID is optional and will be omitted if not set 
 }
 
 func main() {
@@ -19,16 +21,35 @@ func main() {
 
 	log.Println("Setting up routes...")
 
-	router.HandleFunc("/register", register).Methods("POST")
-	router.HandleFunc("/login", login).Methods("POST")
-	router.HandleFunc("/projects", createProject).Methods("POST")
-	router.HandleFunc("/projects/{id}", updateProject).Methods("PUT")
-	router.HandleFunc("/projects", getProjects).Methods("GET")
-	router.HandleFunc("/project/{id}", getProject).Methods("GET")
-	router.HandleFunc("/project/{id}", deleteProject).Methods("DELETE")
+	// Alice is a middleware stack that allows you to compose multiple middleware handlers
+	// It is used to apply middleware to the router
+	
+	router.Handle("/register", alice.New(loggingMiddleware).ThenFunc(register)).Methods("POST")
+	
+	router.Handle("/login", alice.New(loggingMiddleware).ThenFunc(login)).Methods("POST")
+	
+	router.Handle("/projects", alice.New(loggingMiddleware).ThenFunc(createProject)).Methods("POST")
+	
+	router.Handle("/projects/{id}", alice.New(loggingMiddleware).ThenFunc(updateProject)).Methods("PUT")
+	
+	router.Handle("/projects", alice.New(loggingMiddleware).ThenFunc(getProjects)).Methods("GET")
+	
+	router.Handle("/project/{id}", alice.New(loggingMiddleware).ThenFunc(getProject)).Methods("GET")
+	
+	router.Handle("/project/{id}", alice.New(loggingMiddleware).ThenFunc(deleteProject)).Methods("DELETE")
+	
  
 	log.Println("Listening on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+// Make some middleware for logging
+// What it does is basically takes the next handler that has to be done, does it's thing and then basses it further
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		next.ServeHTTP(w, r)
+	})
 }
 
 // register
@@ -51,8 +72,10 @@ func createProject(w http.ResponseWriter, r *http.Request) {
 
 // updateProject
 func updateProject(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // Extracting the variables from the request URL
+	id := vars["id"] // Using the ID variable
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from updateProject"})
+	json.NewEncoder(w).Encode(RouteResponse{Message: "Updating project", ID: id})
 }
 
 // getProjects
@@ -63,12 +86,16 @@ func getProjects(w http.ResponseWriter, r *http.Request) {
 
 // getProject
 func getProject(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // Extracting the variables from the request URL
+	id := vars["id"] // Using the ID variable
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from getProject"})
+	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from getProject", ID: id})
 }
 
 // deleteProject
 func deleteProject(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // Extracting the variables from the request URL
+	id := vars["id"] // Using the ID variable
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from deleteProject"})
+	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from deleteProject", ID: id})
 }
